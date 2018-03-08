47faaa5628b1c44d98063b6a91caec364e5e56cb
douglase <edouglas@mit.edu>
2018-03-08 15:43:12 -0500

    test script

---
+++ test_map.py
@@ -0,0 +1,43 @@
+import dustmap
+import numpy as np
+import astropy.units as u
+inputfile="dustmap/sample_inputfile.dat"
+distance = 10.                #pc
+fov = [1.*u.arcsecond]                 #mas
+pixelsize=1.0*u.arcsecond
+numpixels = 200
+datatype = 1
+inclination = 60. #degrees
+longitude = 90. #degrees
+pa = 45. #degrees
+opticaldepth = 1
+rdust = 1.
+thermal = 1
+wavel = np.array([10.],dtype=np.double)
+Lstar = 1.
+Tstar = 5778.
+kurucz = 1
+logg=4.5
+composition = 'astrosil'
+scattered = 1
+hg = 1
+g = 0.1
+iwa=0.05*u.arcsecond
+tsublimate=1e10
+fstar= np.array([0.],dtype=np.double)
+scaling= np.empty(1,dtype=np.int)
+hist=np.empty([numpixels,numpixels],dtype=np.double)
+dustmap.dustmap_func(inputfile,
+          hist,
+          distance,
+          fov,
+          pixelsize,
+          numpixels,
+          #au,
+          #degrees,
+          wavel, #was lambda
+          Tstar,
+          fstar,
+          composition,
+          )
+          #pfunc,
