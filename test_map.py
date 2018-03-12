faf46b3a700ab06f0ad25f924c35a8431b5a1b17
douglase <edouglas@mit.edu>
2018-03-12 12:08:03 -0400

    better demo

--- test_map.py
+++ test_map.py
@@ -1,25 +1,37 @@
 import dustmap
 import numpy as np
 import astropy.units as u
+import astropy.io.fits as fits
 import matplotlib.pyplot as plt
 
 inputfile="dustmap/sample_inputfile.dat"
 distance = 10.                #pc
 fov = [1.*u.arcsecond]                 #mas
-pixelsize=0.05*u.arcsecond
+pixelsize=0.005*u.arcsecond
 numpixels = 200
 datatype = 1
 composition = 'astrosil'
-
-
-
-fstar= np.array([0.],dtype=np.float32)
+inclination = 30. #degrees
+longitude = 90. #degrees
+pa = 0. #;degrees
+opticaldepth = 0
+rdust = 1.
+thermal = 1
+Lstar = 1.
+Tstar = 5778.
+kurucz = 1
+logg = 4.5
+scattered = 1
+thermal = 1
+hg = 1
+g = 0.1
 #scaling= np.array([0.],dtype=np.double)
-wavel = np.array([0.55,5],dtype=np.float32)
+wavel = np.array([.5],dtype=np.float32)
 Tstar = 5778.
 
-hist=np.empty([numpixels,numpixels],dtype=np.double)
+hist=np.zeros([numpixels,numpixels],dtype=np.double)
 print(hist)
+
 dustmap.dustmap_func(inputfile,
           hist,
           distance,
@@ -30,24 +42,32 @@ dustmap.dustmap_func(inputfile,
           #degrees,
           wavel, #was lambda
           Tstar,
-          fstar,
+           np.array([0.],dtype=np.float32),
           composition,
-          scatteredlight_flag=1,
-          thermalemission_flag=1,
+          scatteredlight_flag=scattered,
+          thermalemission_flag=thermal,
           verbose_flag=2,
-          inclination = 60., #degrees
-          longitude = 90., #degrees
-          pa = 45. ,#degrees
-          opticaldepth_flag = 1,
-          rdust = 1.,
-          Lstar = 1.,
-          kurucz = 1,
+          inclination = 40.0, #degrees
+          longitude = longitude, #degrees
+         # pa = pa ,#degrees
+          #opticaldepth_flag = 0,
+          #densityhisto_flag=1,
+          rdust =rdust,
+          Lstar = Lstar,
+          kurucz = kurucz,
+          effrdust=1,
           logg=4.5,
-          HG_flag = 1,
-          HG_g = 0.1,
-          iwa=0.05*u.arcsecond,
+          HG_flag = hg,
+          HG_g = g,
+          iwa=0.00*u.arcsecond,
           Tsublimate=1e10,
+          ncostheta=500,
+          scaling=np.array([1.0],dtype=np.double),
           )
-plt.imshow(hist)
+print(hist)
+hist[0,:]=0 #dustmap is setting first row to 1 ?
+fits.writeto("test.fits",hist,overwrite=True)
+
+plt.imshow(np.array(hist),origin="upper left")
+
 plt.show()
-          #pfunc,
